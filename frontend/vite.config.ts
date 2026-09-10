f97cf9eaf840ed7ffc75777a20f7c93fdb31e498
Srinidhi Karlapalem <karlascolleges@gmail.com>
2026-09-10 19:23:02 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({plugins: [react()], server: {proxy: {"/api": {target: "http://backend:8000", rewrite: path => path.replace(/^\/api/, "")}}}});
+export default defineConfig({
+  plugins: [react()],
+  base: "/Community-Resource-Management-Platform/",
+});
